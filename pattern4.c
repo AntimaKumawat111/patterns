c28c8dc617366d871686d74d702ef36ed2dc2fb8
AntimaKumawat <aktank810>
2024-05-11 22:29:39 +0530

    Add pattern 4

---
+++ pattern4.c
@@ -0,0 +1,29 @@
+// This pattern
+/*
+
+*********
+**** ****
+***   ***
+**     **
+*       *
+
+
+*/
+
+#include <stdio.h>
+int main()
+{
+    int i, j;
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= 9; j++)
+        {
+            if (j >= 4 + i || j <= 6 - i)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
